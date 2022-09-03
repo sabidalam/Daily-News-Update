b7cede7693cea8bc2ed3baf39881ee14f49e8096
sabidalam <Shafkatsaad@gmail.com>
2022-09-03 11:56:31 +0600

    displayed all news

--- js/news.js
+++ js/news.js
@@ -28,7 +28,34 @@ const loadNews = (id) => {
 
 }
 
+const displayNews = allNews => {
+    console.log(allNews);
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = ``;
+    allNews.forEach(news => {
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('col');
+        newsDiv.innerHTML = `
+        <div class="card p-4">
+        <div class="row g-0">
+          <div class="col-12 col-sm-12 col-md-4">
+            <img src="${news.thumbnail_url}" class="card-img-top img-fluid rounded" alt="">
+          </div>  
+          <div class="col-md-8">  
+            <div class="card-body p-5">
+                <h5 class="card-title">${news.title}</h5>
+                <p class="card-text">${news.details.slice(0, 700)}....</p>
+            </div>
+           
+          </div>   
+        </div>
+         </div>               
+        `;
+        newsContainer.appendChild(newsDiv);
 
+    });
+
+}
 
 
 
